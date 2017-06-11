3940d0b6a5ff219192ec2072cbc00e091b9ed748
Narendranath Reddy <thotanarendranathreddy@gmail.com>
2017-06-12 02:24:32 +0530

    Create operations.js

--- routes/operations.js
+++ routes/operations.js
@@ -9,7 +9,7 @@ function ensureAuthenticated(req, res, next) {
   if(req.isAuthenticated()){
     return next();
   } else {
-    //req.flash('error_msg','Please Login to continue');
+    req.flash('error_msg','Please Login to continue');
     res.redirect('/chat/login');
   }
 }
